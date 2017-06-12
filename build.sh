129d436e517949c1ab29afeb76fda2ed7e96b425
Marco Azzalin <azzalin.marco@gmail.com>
2017-06-13 00:11:20 +0100

    Pulling from tag

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-git clone https://github.com/marcoazza/jengalamp.git
-cd jengalamp
+wget https://github.com/marcoazza/jengalamp/archive/v$1.tar.gz
+tar xvzf v$1.tar.gz
+cd jengalamp-$1
 docker build -t jengalamp_led_manager led_manager/
 docker build -t jengalamp_tbot tbot/
 chmod -R +w .git
 cp jenga.yml ..
 cd ..
-rm -r jengalamp
+rm -r jengalamp-$1
